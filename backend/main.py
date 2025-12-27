887b66b81df6a8f043876be8004964c6ab4653d8
sagivt1 <sagiv.talker@gmail.com>
2025-12-27 15:10:57 +0200

    fix backend rabbitmq listen for msg

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import threading
 
+import time
 import pika
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
@@ -7,24 +8,26 @@ from contextlib import asynccontextmanager
 
 def run_consumer():
 
+    while True:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+            channel = connection.channel()
+            channel.queue_declare(queue="health_check", durable=False)
 
-    try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
-        channel = connection.channel()
-        channel.queue_declare(queue="health_check")
 
+            def callback(ch, method, properties, body):
+                print(f"[*] User A Received: {body.decode()}", flush=True)
 
-        def callback(ch, method, properties, body):
-            print(f"User A Received: {body.decode()}")
+            channel.basic_consume(
+                queue="health_check", on_message_callback=callback, auto_ack=True
+            )
 
-        channel.basic_consume(
-            queue="health_check", on_message_callback=callback, auto_ack=True
-        )
-
-        print("[*] User A: waiting for messages...")
-        channel.start_consuming()
-    except Exception as e:
-        print(f"[!] Connection faild: {e}")
+            print("[*] User A: waiting for messages...")
+            channel.start_consuming()
+        except pika.exceptions.AMQPConnectionError:
+            print("[!] RabbitMQ not ready yet. Retrying in 5s...", flush=True)
+        except Exception as e:
+            print(f"[!] Connection faild: {e}", flush=True)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -37,4 +40,4 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
-    return {"service": "lumina backend", "status": "OK"}
+    return {"service": "lumina backend", "status": "OK", "lifespan": "enabled"}
