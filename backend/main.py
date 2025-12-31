3da9c11852f1178f4e8e00edd49b71b4c6bea70a
sagivt1 <sagiv.talker@gmail.com>
2025-12-31 19:41:45 +0200

    fix backend/main.py printing user info

--- backend/main.py
+++ backend/main.py
@@ -7,33 +7,35 @@ from fastapi import FastAPI
 
 
 def run_consumer():
-    try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
-        channel = connection.channel()
-        channel.queue_declare(queue="health_check", durable=False)
-
-        def callback(ch, method, properties, body):
-            try:
-                data = json.loads(body)
-
-                user_id = data.get("user", {}).get("sub", "Unknown")
-
-                print(f"[User A] Secure Task recived for user: {user_id}", flush=True)
-                print(f"         Content: {data.get('content')}", flush=True)
-            except json.JSONDecodeError:
-                print("[!] Error: Received malformed JSON message.")
-
-        channel.basic_qos(prefetch_count=1)
-        channel.basic_consume(
-            queue="health_check", on_message_callback=callback, auto_ack=True
-        )
-        print("[*] User A: waiting for messages...")
-        channel.start_consuming()
-
-    except pika.exceptions.AMQPConnectionError:
-        print("[!] RabbitMQ not ready yet. Retrying in 5s...", flush=True)
-    except Exception as e:
-        print(f"[!] Connection faild: {e}", flush=True)
+    while True:
+        try:
+            
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+            channel = connection.channel()
+            channel.queue_declare(queue="health_check", durable=False)
+    
+            def callback(ch, method, properties, body):
+                try:
+                    data = json.loads(body)
+    
+                    user_id = data.get("user", {}).get("sub", "Unknown")
+    
+                    print(f"[User A] Secure Task recived for user: {user_id}", flush=True)
+                    print(f"         Content: {data.get('content')}", flush=True)
+                except json.JSONDecodeError:
+                    print("[!] Error: Received malformed JSON message.")
+    
+            channel.basic_qos(prefetch_count=1)
+            channel.basic_consume(
+                queue="health_check", on_message_callback=callback, auto_ack=True
+            )
+            print("[*] User A: waiting for messages...")
+            channel.start_consuming()
+    
+        except pika.exceptions.AMQPConnectionError:
+            print("[!] RabbitMQ not ready yet. Retrying in 5s...", flush=True)
+        except Exception as e:
+            print(f"[!] Connection faild: {e}", flush=True)
 
 
 @asynccontextmanager
